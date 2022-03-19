cc966ae0670b3f833303b0072f77bd047b80975b
bader.b <baderbadr.996@gmail.com>
2022-03-19 20:04:34 +0200

    im[lement pier controller

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PierController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,5 +28,7 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
 
     Route::get('/admin/notifications', [AdminController::class, 'getNotifications']);
 
-    Route::resource('/payload-types',\App\Http\Controllers\PayloadTypeController::class);
+    Route::resource('/payload-types', \App\Http\Controllers\PayloadTypeController::class);
+
+    Route::resource('/piers', PierController::class);
 });
