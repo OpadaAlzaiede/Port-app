6188e08d2a9f0991b53b8a43d66e5e7d0aba42f2
bader.b <baderbadr.996@gmail.com>
2022-04-22 13:56:24 +0300

    add database constatns

--- app/constants/DataBaseConstants.php
+++ app/constants/DataBaseConstants.php
@@ -6,6 +6,28 @@ namespace App\constants;
 
 class DataBaseConstants
 {
+    const STATUSES = [
+        "IN_PROGRESS" => 1,
+        "DONE" => 2,
+        "CANCELED" => 3
+    ];
 
+    const WAYS = [
+        "FORWARD" => 1,
+        "BACKWARD" => 2,
+        "HOLDING" => 3
+    ];
 
+    const IS_SERVED_YES = 1;
+    const IS_SERVED_NO = 0;
+
+    public function getStatusesArr()
+    {
+        return self::STATUSES;
+    }
+
+    public function getWaysArr()
+    {
+        return self::WAYS;
+    }
 }
